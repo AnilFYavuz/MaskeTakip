c40193809a6c32776f5bb285f5a5a7818d7e6d96
fruit <fruit@DESKTOP-HBDSGJF>
2024-02-10 15:11:22 +0300

    Add project files.

---
+++ Workaround/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Business.Concrete;
+using Entities.Concrete;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FirstName = "ANIL FURKAN";
+            person1.LastName = "YAVUZ";
+            person1.DateOfBirthYear = 1994;
+            person1.NationalIdentity = 123;
+
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+            Console.ReadLine();
+        }
+
+        static void SelamVer()
+        {
+            Console.WriteLine("Merhaba");
+        }
+
+    }
+
+}
